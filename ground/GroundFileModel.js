d935f1f0108bdfaa10bbe74b3852edbb99f6d568
Andy <yzhao@twitter.com>
2013-11-03 20:34:14 -0800

    Add GroundFileModel.

---
+++ ground/GroundFileModel.js
@@ -0,0 +1,74 @@
+var GroundFileModel, GroundModel, fs, mongodb, _ref,
+  __hasProp = {}.hasOwnProperty,
+  __extends = function(child, parent) { for (var key in parent) { if (__hasProp.call(parent, key)) child[key] = parent[key]; } function ctor() { this.constructor = child; } ctor.prototype = parent.prototype; child.prototype = new ctor(); child.__super__ = parent.prototype; return child; },
+  __slice = [].slice;
+
+fs = require('fs');
+
+mongodb = require('mongodb');
+
+GroundModel = require('./ground/GroundModel');
+
+GroundFileModel = (function(_super) {
+  __extends(GroundFileModel, _super);
+
+  function GroundFileModel() {
+    _ref = GroundFileModel.__super__.constructor.apply(this, arguments);
+    return _ref;
+  }
+
+  GroundFileModel.collection = 'fs.files';
+
+  GroundFileModel.methods = {
+    read: function() {
+      var cb, options, _i,
+        _this = this;
+      options = 2 <= arguments.length ? __slice.call(arguments, 0, _i = arguments.length - 1) : (_i = 0, []), cb = arguments[_i++];
+      return this.ground._db(function(err, db) {
+        return mongodb.GridStore.read(db, new mongodb.ObjectID(_this.id), cb);
+      });
+    }
+  };
+
+  GroundFileModel.statics = {
+    writeFile: function() {
+      var cb, file, options, _i, _ref1;
+      file = arguments[0], options = 3 <= arguments.length ? __slice.call(arguments, 1, _i = arguments.length - 1) : (_i = 1, []), cb = arguments[_i++];
+      options = (_ref1 = options[0]) != null ? _ref1 : {};
+      if (options.id == null) {
+        options.id = new mongodb.ObjectID();
+      }
+      return this.ground._db(function(err, db) {
+        var gridStore;
+        gridStore = new mongodb.GridStore(db, options.id, 'w');
+        return gridStore.writeFile(file, function(err, gridStore) {
+          if (err != null) {
+            return cb(err);
+          } else {
+            return gridStore.close(cb);
+          }
+        });
+      });
+    },
+    writeBuffer: function() {
+      var buf, cb, options, _i, _ref1;
+      buf = arguments[0], options = 3 <= arguments.length ? __slice.call(arguments, 1, _i = arguments.length - 1) : (_i = 1, []), cb = arguments[_i++];
+      options = (_ref1 = options[0]) != null ? _ref1 : {};
+      if (options.id == null) {
+        options.id = new mongodb.ObjectID();
+      }
+      return this.ground._db(function(err, db) {
+        var gridStore;
+        gridStore = new mongodb.GridStore(db, options.id, 'w');
+        return gridStore.open(function() {
+          return gridStore.write(buf, function() {
+            return gridStore.close(cb);
+          });
+        });
+      });
+    }
+  };
+
+  return GroundFileModel;
+
+})(GroundModel);
---
+++ src/ground/GroundFileModel.coffee
@@ -0,0 +1,37 @@
+# Author: andyzhau@nodeswork.com (Andy Zhau)
+
+fs = require 'fs'
+mongodb = require 'mongodb'
+GroundModel = require './ground/GroundModel'
+
+class GroundFileModel extends GroundModel
+
+  @collection: 'fs.files'
+
+  @methods:
+
+    read: (options..., cb) ->
+      @ground._db (err, db) =>
+        mongodb.GridStore.read db, new mongodb.ObjectID(@id), cb
+
+  @statics:
+
+    # options:
+    #   id: fileId
+    writeFile: (file, options..., cb) ->
+      options = options[0] ? {}
+      options.id ?= new mongodb.ObjectID()
+      @ground._db (err, db) ->
+        gridStore = new mongodb.GridStore(db, options.id, 'w')
+        gridStore.writeFile file, (err, gridStore) ->
+          if err? then cb err
+          else gridStore.close cb
+
+    writeBuffer: (buf, options..., cb) ->
+      options = options[0] ? {}
+      options.id ?= new mongodb.ObjectID()
+      @ground._db (err, db) ->
+        gridStore = new mongodb.GridStore(db, options.id, 'w')
+        gridStore.open ->
+          gridStore.write buf, ->
+            gridStore.close cb
